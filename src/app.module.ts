f283f4ff15e771678e21562a48927ad3937016b0
Kristian Duda <kristian.duda@extropy.sk>
2026-02-11 13:25:20 +0100

    wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ZodSerializerInterceptor, ZodValidationPipe } from 'nestjs-zod';
-import config from 'src/config';
+import config, { Config } from 'src/config';
 import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
-import { auth } from 'src/auth';
+import { createAuth } from 'src/auth';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, load: [config] }),
-    AuthModule.forRoot({ auth }),
+    AuthModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<Config, true>) => ({
+        auth: createAuth({
+          mongodbUri: configService.get('mongodbUri'),
+          betterAuthSecret: configService.get('betterAuthSecret'),
+          betterAuthUrl: configService.get('betterAuthUrl'),
+        }),
+      }),
+    }),
   ],
   controllers: [AppController],
   providers: [
--- src/auth.ts
+++ src/auth.ts
@@ -1,27 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { betterAuth } from 'better-auth';
 import { mongodbAdapter } from 'better-auth/adapters/mongodb';
 import { bearer } from 'better-auth/plugins';
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/extropy',
-);
-const db = client.db();
+export function createAuth(config: {
+  mongodbUri: string;
+  betterAuthSecret: string;
+  betterAuthUrl: string;
+}) {
+  const client = new MongoClient(config.mongodbUri);
+  const db = client.db();
 
-export const auth = betterAuth({
-  database: mongodbAdapter(db, { client, transaction: false }),
-  basePath: '/api/auth',
-  secret: process.env.BETTER_AUTH_SECRET,
-  baseURL: process.env.BETTER_AUTH_URL,
-  emailAndPassword: {
-    enabled: true,
-  },
-  advanced: {
-    disableCSRFCheck: true,
-  },
-  plugins: [bearer()],
-});
+  return betterAuth({
+    database: mongodbAdapter(db, { client, transaction: false }),
+    basePath: '/auth',
+    secret: config.betterAuthSecret,
+    baseURL: config.betterAuthUrl,
+    emailAndPassword: {
+      enabled: true,
+    },
+    advanced: {
+      disableCSRFCheck: true,
+    },
+    plugins: [bearer()],
+  });
+}
 
-export type Auth = typeof auth;
+export type Auth = ReturnType<typeof createAuth>;
